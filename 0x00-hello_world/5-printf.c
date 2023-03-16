8f37e1a559b49a10939c56be6daadd7227a8762f
Benouraida Zakaria <benouraida.zakaria@gmail.com>
2023-03-16 15:38:24 -0700

    5-hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints with printf function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

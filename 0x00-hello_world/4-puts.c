d11305abf95373857a09c5c6976a135639a36dab
Benouraida Zakaria <benouraida.zakaria@gmail.com>
2023-03-16 15:22:51 -0700

    edited puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main
+ *
+ * Description  A program that prints with put function 
+ * 
+ * Return: Always 0 
+ **/
 int main(void) 
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
 }

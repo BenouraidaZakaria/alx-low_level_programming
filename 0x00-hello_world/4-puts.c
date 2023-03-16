f85e0a1d3c03f8b862d9f95edc2d4ba6e0660726
Benouraida Zakaria <benouraida.zakaria@gmail.com>
2023-03-16 15:32:24 -0700

    made file betty style compatible

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Description:  A program that prints with put function 
- * 
- * Return: Always 0 
- **/
-
-int main(void) 
+ * Description: A program that prints with put function
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
